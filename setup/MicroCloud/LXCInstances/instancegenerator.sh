5f1a6e151f64c21e91a5f8378aa60263f9a8d5fb
alfred <git.hub@slainte.at>
2025-09-18 22:02:10 +0200

    no ssh-keys in config:       setup/MicroCloud/InstanceGenerator/template/profile/default/config/cloud-init.user-data/users.yaml no ssh-keys in config:       setup/MicroCloud/InstanceGenerator/template/profile/infrastructure/config/cloud-init.user-data/users.yaml no ssh-keys in config:       setup/MicroCloud/LXCInstances/ansible/ansible.yaml no ssh-keys in config:       setup/MicroCloud/LXCInstances/ansible/template/files/id_ed25519 no ssh-keys in config:       setup/MicroCloud/LXCInstances/ansible/template/files/id_ed25519.pub no ssh-keys in config:       setup/MicroCloud/LXCInstances/ansible/template/write_files.yaml no ssh-keys in config:       setup/MicroCloud/LXCInstances/demoalpine/demoalpine.yaml no ssh-keys in config:       setup/MicroCloud/LXCInstances/demodirectory/demodirectory.yaml no ssh-keys in config:       setup/MicroCloud/LXCInstances/demostandard/demostandard.yaml mass generation:     setup/MicroCloud/LXCInstances/instancegenerator.sh

---
+++ setup/MicroCloud/LXCInstances/instancegenerator.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+############################################################################################
+# Instance Generator
+############################################################################################
+#shopt -o -s errexit # Terminates  the shell script if a command returns an error code.
+#shopt -o -s xtrace  # Displays each command before it is executed.
+shopt -o -s nounset  # No Variables without definition
+
+#######
+# Generate all the images
+#######
+md=$(pwd)
+cd ansible
+./ansible_instancegenerator.sh
+cd ${md}
+cd demoalpine
+./demoalpine_instancegenerator.sh
+cd ${md}
+cd demodirectory
+./demodirectory_instancegenerator.sh
+cd ${md}
+cd demoreplace
+./demoreplace_instancegenerator.sh
+cd ${md}
+cd demostandard
+./demostandard_instancegenerator.sh
+cd ${md}
+#
