be48dd5d59e28543ae051f65934f447822af947d
alfred <git.hub@slainte.at>
2025-10-05 08:29:10 +0200

    Secrets at rest:       setup/Setup.sh

--- setup/Setup.sh
+++ setup/Setup.sh
@@ -16,10 +16,9 @@ ${indir}/MicroK8SInit/MicroK8SInit.sh
 ${indir}/MicroK8S_Install/MicroK8S_Install.sh
 ${indir}/MicroK8S_Install/MicroK8S_Docker.sh
 ${indir}/MicroK8S_Install/MicroK8S_Modifications.sh
-${indir}/dar_secrets/dar_secrets.sh
+${indir}/dar_secrets/dar_secrets_create.sh
 sudo usermod -a -G microk8s ${USER}
 sudo chown -f -R ${USER} ~/.kube
-${indir}/alias.sh
 #
 cat <<EOF
 #############################################################################################
@@ -29,7 +28,9 @@ cat <<EOF
 # Log out and in again
 #
 # copy /var/snap/microk8s/current/args/encryption-config to all other nodes
-# 
+# executing dar_secrets.sh on all nodes
+# check with dar_secrets_check.sh on all nodes
+#
 # Then form a cluster with
 # microk8s add-node on the first node
 # microk8s join <ipaddress>:25000/<token> on the other nodes
