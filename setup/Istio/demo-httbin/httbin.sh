d053ffaa978d43c36ae56fcfa1c4672b90999a64
alfred <git.hub@slainte.at>
2025-12-25 14:39:45 +0100

    mtls example:     setup/Istio/demo-httbin/README.md mtls example:     setup/Istio/demo-httbin/httbin.sh

---
+++ setup/Istio/demo-httbin/httbin.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+################################################################################
+#
+# Install httpbin sample
+# See https://istio.io/latest/docs/tasks/traffic-management/ingress/secure-ingress/
+################################################################################
+shopt -o -s errexit   #—Terminates  the shell script if a command returns an error code.
+#shopt -o -s xtrace    #—Displays each command before it is executed.
+shopt -o -s nounset   #-No Variables without definition
+set -euo pipefail
+
+export NAMESPACE="bookinfo"
+export istio_dir="/opt/istio-installation/istio-1.28.1"
+
+# Deploy httpbin sample - this is with the standard Istio API
+echo "Deploying Bookinfo request routing in namespace '${NAMESPACE}'..."
+kubectl apply --namespace ${NAMESPACE} -f ${istio_dir}/samples/httpbin/httpbin.yaml
+kubectl apply --namespace ${NAMESPACE} -f ${istio_dir}/samples/helloworld/helloworld.yaml -l service=helloworld
+kubectl apply --namespace ${NAMESPACE} -f ${istio_dir}/samples/helloworld/helloworld.yaml -l version=v1
+
+
+# Create a root certificate and private key to sign the certificates for your services:
+rm -rf example_certs1 example_certs2
+mkdir example_certs1
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=example Inc./CN=example.com' -keyout example_certs1/example.com.key -out example_certs1/example.com.crt
+
+# Generate a certificate and a private key for httpbin.example.com:
+
+openssl req -out example_certs1/httpbin.example.com.csr -newkey rsa:2048 -nodes -keyout example_certs1/httpbin.example.com.key -subj "/CN=httpbin.example.com/O=httpbin organization"
+openssl x509 -req -sha256 -days 365 -CA example_certs1/example.com.crt -CAkey example_certs1/example.com.key -set_serial 0 -in example_certs1/httpbin.example.com.csr -out example_certs1/httpbin.example.com.crt
+
+# Create a second set of the same kind of certificates and keys:
+
+mkdir example_certs2
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=example Inc./CN=example.com' -keyout example_certs2/example.com.key -out example_certs2/example.com.crt
+openssl req -out example_certs2/httpbin.example.com.csr -newkey rsa:2048 -nodes -keyout example_certs2/httpbin.example.com.key -subj "/CN=httpbin.example.com/O=httpbin organization"
+openssl x509 -req -sha256 -days 365 -CA example_certs2/example.com.crt -CAkey example_certs2/example.com.key -set_serial 0 -in example_certs2/httpbin.example.com.csr -out example_certs2/httpbin.example.com.crt
+
+# Generate a certificate and a private key for helloworld.example.com:
+
+openssl req -out example_certs1/helloworld.example.com.csr -newkey rsa:2048 -nodes -keyout example_certs1/helloworld.example.com.key -subj "/CN=helloworld.example.com/O=helloworld organization"
+openssl x509 -req -sha256 -days 365 -CA example_certs1/example.com.crt -CAkey example_certs1/example.com.key -set_serial 1 -in example_certs1/helloworld.example.com.csr -out example_certs1/helloworld.example.com.crt
+
+# Generate a client certificate and private key:
+
+openssl req -out example_certs1/client.example.com.csr -newkey rsa:2048 -nodes -keyout example_certs1/client.example.com.key -subj "/CN=client.example.com/O=client organization"
+openssl x509 -req -sha256 -days 365 -CA example_certs1/example.com.crt -CAkey example_certs1/example.com.key -set_serial 1 -in example_certs1/client.example.com.csr -out example_certs1/client.example.com.crt
+
+# Create Kubernetes secrets to hold the certificates and keys:
+kubectl -n ${NAMESPACE} delete secret httpbin-credential
+kubectl create -n ${NAMESPACE} secret generic httpbin-credential \
+  --from-file=tls.key=example_certs1/httpbin.example.com.key \
+  --from-file=tls.crt=example_certs1/httpbin.example.com.crt \
+  --from-file=ca.crt=example_certs1/example.com.crt
+
+cat <<EOF | kubectl apply -f -
+apiVersion: networking.istio.io/v1
+kind: Gateway
+metadata:
+  name: mygateway
+  namespace: ${NAMESPACE}
+spec:
+  selector:
+    istio: ingressgateway # use istio default ingress gateway
+  servers:
+  - port:
+      number: 443
+      name: https
+      protocol: HTTPS
+    tls:
+      mode: MUTUAL
+      credentialName: httpbin-credential # must be the same as secret
+    hosts:
+    - httpbin.example.com
+EOF
+
+echo "Get Ingress Gateway details for accessing the application"
+export INGRESS_NAME=istio-ingressgateway
+export INGRESS_NS=istio-system
+
+export INGRESS_HOST=$(kubectl -n "$INGRESS_NS" get service "$INGRESS_NAME" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+export INGRESS_PORT=$(kubectl -n "$INGRESS_NS" get service "$INGRESS_NAME" -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
+export SECURE_INGRESS_PORT=$(kubectl -n "$INGRESS_NS" get service "$INGRESS_NAME" -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
+export TCP_INGRESS_PORT=$(kubectl -n "$INGRESS_NS" get service "$INGRESS_NAME" -o jsonpath='{.spec.ports[?(@.name=="tcp")].port}')
+export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+echo "Gateway URL: $GATEWAY_URL"
+
+curl -v -HHost:httpbin.example.com --resolve "httpbin.example.com:$SECURE_INGRESS_PORT:$INGRESS_HOST" \
+  --cacert example_certs1/example.com.crt --cert example_certs1/client.example.com.crt --key example_certs1/client.example.com.key \
+  "https://httpbin.example.com:$SECURE_INGRESS_PORT/status/418"
+
+
+exit 0
