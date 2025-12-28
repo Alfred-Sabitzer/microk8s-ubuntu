bb508e3b4e4c398678c1fa25e95e5a57328a6be6
alfred <git.hub@slainte.at>
2025-12-28 21:01:49 +0100

    generate certificates:       setup/ca/ca.sh

--- setup/ca/ca.sh
+++ setup/ca/ca.sh
@@ -40,22 +40,28 @@ done
 
 sleep 10
 
-# Cleanup old certificates
-sudo rm -f /var/snap/microk8s/current/certs/k8s-root-secret.crt
-sudo rm -f /usr/local/share/ca-certificates/k8s-root-secret.crt
+sudo mkdir -p /usr/local/share/ca-certificates/
+# Delete specific secrets first to avoid dangling resources
+echo "Deleting specific secrets ..."
 sudo rm -f /usr/local/share/ca-certificates/ca.crt
+cat ca.yaml | grep 'secretName:' | awk '{print $2}' | while read -r secret_name; do
+  echo "Deleting secret $secret_name in namespace cert-manager ..."
+# Cleanup old certificates
+  kubectl delete secret -n cert-manager "$secret_name" --ignore-not-found=true
+  sudo rm -f /var/snap/microk8s/current/certs/$secret_name.crt
 # Extract the CA certificate from the Kubernetes secret and save it to a file
 # Sefsigned Certificates are not trusted by default
 # https://microk8s.io/docs/ssl-certs
 # https://collabnix.com/installing-prometheus-on-microk8s-in-2025-a-step-by-step-guide/
-kubectl get secret \
-    -n cert-manager k8s-root-secret \
-    -o jsonpath="{.data.ca\.crt}" | base64 -d > /var/snap/microk8s/current/certs/k8s-root-secret.crt
+  kubectl get secret \
+    -n cert-manager $secret_name \
+    -o jsonpath="{.data.ca\.crt}" | base64 -d > /var/snap/microk8s/current/certs/$secret_name.crt  
 # Copy the CA certificate to the system's trusted CA store
-echo "Copying CA certificate to system's trusted CA store..."
-sudo mkdir -p /usr/local/share/ca-certificates/
-sudo cp /var/snap/microk8s/current/certs/k8s-root-secret.crt /usr/local/share/ca-certificates/
+  sudo cp /var/snap/microk8s/current/certs/$secret_name.crt /usr/local/share/ca-certificates/
+done
 sudo cp /var/snap/microk8s/current/certs/ca.crt /usr/local/share/ca-certificates/
+
+echo "Updating CA certificates..."
 sudo update-ca-certificates
 
 echo "CA resources applied successfully."
