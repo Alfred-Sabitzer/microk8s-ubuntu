d041ec5ff9a681fedf2f8802e44e0da7d629dbf5
alfred <git.hub@slainte.at>
2026-02-07 10:56:09 +0100

    adopt values.yaml:   applications/openBao/openBao.sh

--- applications/openBao/openBao.sh
+++ applications/openBao/openBao.sh
@@ -40,19 +40,11 @@ WAIT_SECONDS="${WAIT_SECONDS:-180}"
 RETRY_ATTEMPTS=5
 RETRY_DELAY=5
 
-check_cmd
-
-echo "Ensuring sudo microk8s is ready..."
-sudo microk8s status --wait-ready
-
-# Sync as Kubernetes secret	
-# Secret Auto rotation
 sudo microk8s helm uninstall secrets-store-csi-driver --namespace ${NAMESPACE} --ignore-not-found=true
 sudo microk8s kubectl delete clusterrole secretproviderclasses-admin-role --ignore-not-found=true || true
 
 echo "Uninstalling any existing OpenBao release..."
 sudo microk8s helm uninstall openbao --namespace ${NAMESPACE} --ignore-not-found=true
-
 
 echo ""
 echo "Finding YAML files in $SCRIPT_DIR..."
@@ -112,34 +104,19 @@ else
 fi
 
 cat <<EOF > "/tmp/openbao-values.yaml"
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-# Available parameters and their default values for the OpenBao chart.
-
-global:
-  # -- enabled is the master enabled switch. Setting this to true or false
-  # will enable or disable all the components within this chart by default.
-  enabled: true
-
-  # -- The namespace to deploy to. Defaults to the `helm` installation namespace.
-  namespace: "${NAMESPACE}"
-
-
+# https://github.com/openbao/openbao-helm/blob/main/charts/openbao/values.yaml
 server:
 
-  extraVolumes:
+  volumes:
     - name: softhsm-lib
       persistentVolumeClaim:
         claimName: softhsm-pvc
 
-  extraVolumeMounts:
+  volumeMounts:
     - name: softhsm-lib
       mountPath: /usr/local/lib/softhsm/libsofthsm2.so
       subPath: libsofthsm2.so
-
-  extraEnvironmentVars:
-    PKCS11_PIN: "${K8S_OPENBAO_USER_PIN}"
+      readOnly: true
 
   config: |
     listener "tcp" {
@@ -158,118 +135,6 @@ server:
       key_label      = "openbao-unseal-key"
       hmac_key_label = "openbao-hmac-key"
     }
-
-# OpenBao is able to collect and publish various runtime metrics.
-# Enabling this feature requires setting adding `telemetry{}` stanza to
-# the OpenBao configuration. There are a few examples included in the `config` sections above.
-#
-# For more information see:
-# https://openbao.org/docs/configuration/telemetry
-# https://openbao.org/docs/internals/telemetry
-serverTelemetry:
-  # Enable support for the Prometheus Operator. If authorization is not required for
-  # OpenBao's metrics endpoint, the following OpenBao server `telemetry{}` config must be included
-  # in the `listener "tcp"{}` stanza
-  #  telemetry {
-  #    unauthenticated_metrics_access = "true"
-  #  }
-  #
-  # See the `standalone.config` for a more complete example of this.
-  #
-  # In addition, a top level `telemetry{}` stanza must also be included in the OpenBao configuration:
-  #
-  # example:
-  #  telemetry {
-  #    prometheus_retention_time = "30s"
-  #    disable_hostname = true
-  #  }
-  #
-  # Configuration for monitoring the OpenBao server.
-  serviceMonitor:
-    # The Prometheus operator *must* be installed before enabling this feature,
-    # if not the chart will fail to install due to missing CustomResourceDefinitions
-    # provided by the operator.
-    #
-    # Instructions on how to install the Helm chart can be found here:
-    #  https://github.com/prometheus-community/helm-charts/tree/main/charts/kube-prometheus-stack
-    # More information can be found here:
-    #  https://github.com/prometheus-operator/prometheus-operator
-    #  https://github.com/prometheus-operator/kube-prometheus
-
-    # Enable deployment of the OpenBao Server ServiceMonitor CustomResource.
-    enabled: true
-
-    # Selector labels to add to the ServiceMonitor.
-    # When empty, defaults to:
-    #  release: prometheus
-    selectors: {}
-
-    # Interval at which Prometheus scrapes metrics
-    interval: 30s
-
-    # Timeout for Prometheus scrapes
-    scrapeTimeout: 10s
-
-    # tlsConfig used for scraping the Vault metrics API.
-    tlsConfig: {}
-
-    # authorization used for scraping the Vault metrics API.
-    authorization: {}
-
-    # scrapeClass to be used by the serviceMonitor
-    scrapeClass: ""
-
-  prometheusRules:
-    # The Prometheus operator *must* be installed before enabling this feature,
-    # if not the chart will fail to install due to missing CustomResourceDefinitions
-    # provided by the operator.
-
-    # Deploy the PrometheusRule custom resource for AlertManager based alerts.
-    # Requires that AlertManager is properly deployed.
-    enabled: true
-
-    # Selector labels to add to the PrometheusRules.
-    # When empty, defaults to:
-    #  release: prometheus
-    selectors: {}
-
-    # Some example rules.
-    rules: []
-     - alert: vault-HighResponseTime
-       annotations:
-         message: The response time of OpenBao is over 500ms on average over the last 5 minutes.
-       expr: vault_core_handle_request{quantile="0.5", namespace="${NAMESPACE}"} > 500
-       for: 5m
-       labels:
-         severity: warning
-     - alert: vault-HighResponseTime
-       annotations:
-         message: The response time of OpenBao is over 1s on average over the last 10 minutes.
-       expr: vault_core_handle_request{quantile="0.5", namespace="${NAMESPACE}"} > 1000
-       for: 10m
-       labels:
-         severity: critical
-
-  grafanaDashboard:
-    # Enable deployment of the OpenBao Grafana dashboard.
-    # https://grafana.com/grafana/dashboards/23725-openbao
-    enabled: true
-
-    # Add `grafana_dashboard: "1"` default label
-    defaultLabel: true
-
-    # Extra labels for dashboard ConfigMap
-    extraLabel: {}
-
-    # Extra annotations for dashboard ConfigMap
-    extraAnnotations: {}
-
-security:
-  pkcs11:
-    enabled: true
-    library: "/usr/local/lib/softhsm/libsofthsm2.so"
-    tokenLabel: "OpenbaoToken"
-    userPin: "${K8S_OPENBAO_USER_PIN}"
 
 EOF
 
