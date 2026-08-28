3853837d892a13a6966313d14fb41b5cd14188e6
alfred <git.hub@slainte.at>
2026-08-28 17:16:33 +0200

    dump config:   applications/harbor/scripts/get_config.sh not needed:    applications/harbor/scripts/get_list_of_projects.sh not needed:    applications/harbor/scripts/projects.json

---
+++ applications/harbor/scripts/get_config.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+############################################################################################
+#
+# Get List of Projects from Harbor https://github.com/goharbor/harbor/discussions/17426
+#
+############################################################################################
+#shopt -o -s errexit    #—Terminates  the shell script  if a command returns an error code.
+#shopt -o -s xtrace #—Displays each command before it is executed.
+shopt -o -s nounset #-No Variables without definition
+
+get_specific_project() {
+    project_name="$1"
+    echo "Getting project: ${project_name}"
+    curl --cert /etc/containers/certs.d/harbor.test.slainte.at/client.cert \
+        --key /etc/containers/certs.d/harbor.test.slainte.at/client.key \
+        --cacert /etc/containers/certs.d/harbor.test.slainte.at/ca.crt  \
+        -k -X 'GET' \
+        -u "admin:${HARBOR_ADMIN_PASSWORD}" \
+        -H 'accept: application/json' \
+        -H 'X-Is-Resource-Name: false' \
+        "https://harbor.test.slainte.at/api/v2.0/projects/${project_name}" \
+    >${project_name}.json 2>${project_name}.err 
+}
+
+get_specific_user() {
+    user_name="$1"
+    echo "Getting user: ${user_name}"
+    curl --cert /etc/containers/certs.d/harbor.test.slainte.at/client.cert \
+        --key /etc/containers/certs.d/harbor.test.slainte.at/client.key \
+        --cacert /etc/containers/certs.d/harbor.test.slainte.at/ca.crt  \
+        -k -X 'GET' \
+        -H 'accept: application/json' \
+        -u "admin:${HARBOR_ADMIN_PASSWORD}" \
+        'https://harbor.test.slainte.at/api/v2.0/users/search?&username=developer' \
+    >${user_name}.json 2>${user_name}.err
+#
+    user_id=$(cat ${user_name}.json | jq '.[] | .user_id')
+    curl --cert /etc/containers/certs.d/harbor.test.slainte.at/client.cert \
+        --key /etc/containers/certs.d/harbor.test.slainte.at/client.key \
+        --cacert /etc/containers/certs.d/harbor.test.slainte.at/ca.crt  \
+        -k -X 'GET' \
+        -H 'accept: application/json' \
+        -u "admin:${HARBOR_ADMIN_PASSWORD}" \
+        "https://harbor.test.slainte.at/api/v2.0/users/${user_id}" \
+    >${user_name}_profile.json 2>${user_name}_profile.err 
+
+}
+
+main() {
+    get_specific_project "test"
+    get_specific_project "dockerhub"
+    get_specific_user "developer"
+}
+
+main "$@"
